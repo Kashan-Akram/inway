129bf5479e10e8a16dcc6e77691f885af28b5d1d
Kashan-Akram <kashan123akram456gmail.com>
2023-07-26 03:03:29 +0530

    x

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+// Replace 'your_predefined_number' with the actual WhatsApp Business number, including the country code.
+const whatsappNumber = "919667851118";
+
+document
+  .getElementById("whatsapp_button")
+  .addEventListener("click", function () {
+    // Replace 'your_predefined_message' with the default message you want to send.
+    const defaultMessage = "Hello! I have a question!!!";
+    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+      defaultMessage
+    )}`;
+
+    window.open(whatsappUrl, "_blank");
+  });
